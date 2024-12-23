2af34ec0e4fc0bf9873e2c410ddb0fe47dff0ee3
yjyoo6831 <cenabro@naver.com>
2024-12-24 01:05:40 +0900

    test: jenkins2

--- src/main/java/sesac_3rd/sesac_3rd/Sesac3rdApplication.java
+++ src/main/java/sesac_3rd/sesac_3rd/Sesac3rdApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class Sesac3rdApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Sesac3rdApplication.class, args);
 	}
-
 }
